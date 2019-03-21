400e7648eb73780dddcb71a2c9e55f9aee3ea5b5
Md. Rafsan Zaman <mzaman1@stuy.edu>
2019-03-21 10:58:30 -0400

    Second Deque constructor completed!

--- MyDeque.java
+++ MyDeque.java
@@ -3,7 +3,12 @@ public class MyDeque<E>{
   private int size, start, end;
 
   public MyDeque(){  }
-  public MyDeque(int initialCapacity){  }
+  public MyDeque(int initialCapacity){ 
+    data = new E[initialCapacity];
+    size = initialCapacity;
+    start = 0;
+    end = 0;
+  }
   public int size(){ }
   public String toString(){ }
   public void addFirst(E element){ }
