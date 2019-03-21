2dc3ae42e94727baf25f4f145ba07749c44430c6
Md. Rafsan Zaman <mzaman1@stuy.edu>
2019-03-21 10:57:07 -0400

    File created!

---
+++ MyDeque.java
@@ -0,0 +1,15 @@
+public class MyDeque<E>{
+  private E[] data;
+  private int size, start, end;
+
+  public MyDeque(){  }
+  public MyDeque(int initialCapacity){  }
+  public int size(){ }
+  public String toString(){ }
+  public void addFirst(E element){ }
+  public void addLast(E element){ }
+  public E removeFirst(){ }
+  public E removeLast(){ }
+  public E getFirst(){ }
+  public E getLast(){ }
+}
