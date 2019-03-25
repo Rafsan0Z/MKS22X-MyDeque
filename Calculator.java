8fc4279ef303ece9c29ba47bef453d57795bee94
RoboticsRafsan01 <mzaman1@stuy.edu>
2019-03-25 10:55:44 -0400

    Operator boolean helper method added!

--- Calculator.java
+++ Calculator.java
@@ -6,9 +6,16 @@ public class Calculator{
     while(read.hasNext()){
       String line = read.next();
       if(Operator(line)){
-        
+
       }
     }
+  }
+
+  private static boolean Operator(String g){
+    if(g == "+" || g == "-" || g == "%" || g == "*" || g == "/"){
+      return true;
+    }
+    else{return false;}
   }
 
 
