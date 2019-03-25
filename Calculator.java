e0957bfbf932b334fddb7814bcd50b6385fc57e9
RoboticsRafsan01 <mzaman1@stuy.edu>
2019-03-25 11:00:29 -0400

    eval updated!

--- Calculator.java
+++ Calculator.java
@@ -2,18 +2,21 @@ public class Calculator{
 
 
   public static double eval(String s){
+    double result = 0;
     Scanner read  = new Scanner(s);
     int one = 0;
     int two = 0;
     while(read.hasNext()){
       String line = read.next();
       if(Operator(line)){
+        result += Operate(one,two,line);
       }
       else{
         if(one == 0){one = Integer.parseInt(line);}
         if(two == 0){two = Integer.parseInt(line);}
       }
     }
+    return result;
   }
 
   private static boolean Operator(String g){
@@ -21,6 +24,10 @@ public class Calculator{
       return true;
     }
     else{return false;}
+  }
+
+  private static int Operate(int one, int two, String line){
+
   }
 
 
