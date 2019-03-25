9e5b3d88ac9f6fa3e8a0305d66b2ad91cbe087ec
RoboticsRafsan01 <mzaman1@stuy.edu>
2019-03-25 10:52:53 -0400

    eval edited!

--- Calculator.java
+++ Calculator.java
@@ -4,7 +4,10 @@ public class Calculator{
   public static double eval(String s){
     Scanner read  = new Scanner(s);
     while(read.hasNext()){
-      
+      String line = read.next();
+      if(Operator(line)){
+        
+      }
     }
   }
 
