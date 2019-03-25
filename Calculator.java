38383ffd2e2d025705fb255fe5f6b3683a9b8df1
RoboticsRafsan01 <mzaman1@stuy.edu>
2019-03-25 10:58:42 -0400

    eval method updated!

--- Calculator.java
+++ Calculator.java
@@ -3,10 +3,15 @@ public class Calculator{
 
   public static double eval(String s){
     Scanner read  = new Scanner(s);
+    int one = 0;
+    int two = 0;
     while(read.hasNext()){
       String line = read.next();
       if(Operator(line)){
-
+      }
+      else{
+        if(one == 0){one = Integer.parseInt(line);}
+        if(two == 0){two = Integer.parseInt(line);}
       }
     }
   }
