f06912a0a52f8470c7568fe830a20645be3b287e
RoboticsRafsan01 <mzaman1@stuy.edu>
2019-03-25 21:03:20 -0400

    eval method edited!

--- Calculator.java
+++ Calculator.java
@@ -4,6 +4,7 @@ public class Calculator{
   public static double eval(String s){
     double result = 0;
     Scanner read  = new Scanner(s);
+    MyDeque<Double> storage = new MyDeque<Double>();
     int one = 0;
     int two = 0;
     while(read.hasNext()){
@@ -12,8 +13,8 @@ public class Calculator{
         result += Operate(one,two,line);
       }
       else{
-        if(one == 0){one = Integer.parseInt(line);}
-        if(two == 0){two = Integer.parseInt(line);}
+        double box = Double.parseDouble(line);
+        storage.addFirst(box)
       }
     }
     return result;
